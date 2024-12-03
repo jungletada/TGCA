33e2aa872bdfd6b13c2909186c6ceda31bfb1c0c
jungletada <1002955176@qq.com>
2024-12-03 12:38:48 +0900

    run

--- scripts_voc/run_cam.sh
+++ scripts_voc/run_cam.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-GPU=0,1
-NODES=2
+GPU=0
+NODES=1
 
 DATASET=VOC12
 DATACONFIG=data/VOCdevkit/VOC2012/ImageLists
@@ -18,34 +18,34 @@ CRFDIR=crf_mask_train
 
 CUDA_VISIBLE_DEVICES=${GPU}
 
-#============= Train Model ============= #
-OMP_NUM_THREADS=${NODES} \
-torchrun --nproc_per_node=${NODES} --nnodes=1 \
-    train_model.py \
-    --dataset ${DATASET} \
-    --model ${MODELNAME} \
-    --train_list ${TRAINAUGID} \
-    --work_space ${WORKDIR} \
-    --seed 11 \
-    --epoch 27 \
-    --batch_per_gpu 20 \
-    
-# # ============= Make Class Activation Maps of Model ============= #
-# python make_cam.py \
+# #============= Train Model ============= #
+# OMP_NUM_THREADS=${NODES} \
+# torchrun --nproc_per_node=${NODES} --nnodes=1 \
+#     train_model.py \
 #     --dataset ${DATASET} \
 #     --model ${MODELNAME} \
+#     --train_list ${TRAINAUGID} \
 #     --work_space ${WORKDIR} \
-#     --cam_out_dir ${SEGDIR} \
-#     --train_list ${TRAINID} \
-#     --input_size ${INPUTSIZE} \
-#     --checkpoint results_voc/mcta/mcta_deit_small-7458.pth \
-#     # --checkpoint ${WORKDIR}/${MODELNAME}_best.pth \
+#     --seed 11 \
+#     --epoch 27 \
+#     --batch_per_gpu 20 \
+    
+# ============= Make Class Activation Maps of Model ============= #
+python make_cam.py \
+    --dataset ${DATASET} \
+    --model ${MODELNAME} \
+    --work_space ${WORKDIR} \
+    --cam_out_dir ${SEGDIR} \
+    --train_list ${TRAINID} \
+    --input_size ${INPUTSIZE} \
+    --checkpoint results_voc/mcta/mcta-deit-small-voc-7458.pth \
+    # --checkpoint ${WORKDIR}/${MODELNAME}_best.pth \
     
     
-# # ============= Evaluate Class Activation Maps without CRF =============#
-# python eval_cam_crf.py \
-#     --dataset ${DATASET} \
-#     --work_space ${WORKDIR} \
-#     --id_list ${TRAINID} \
-#     --eval_cam_dir ${SEGDIR} \
-#     --curve_threshold \
+# ============= Evaluate Class Activation Maps without CRF =============#
+python eval_cam_crf.py \
+    --dataset ${DATASET} \
+    --work_space ${WORKDIR} \
+    --id_list ${TRAINID} \
+    --eval_cam_dir ${SEGDIR} \
+    --curve_threshold \
--- scripts_voc/run_direct.sh
+++ scripts_voc/run_direct.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-GPU=0,1
-NODES=2
+GPU=0
+NODES=1
 
 SEGDIR=cam_mask_val
 CRFDIR=crf_mask_val
@@ -17,15 +17,15 @@ WORKDIR=results_voc/mcta
 
 CUDA_VISIBLE_DEVICES=${GPU}
 
-# # ============= Make Class Activation Maps of Model ============= #
-# python make_cam.py \
-#     --dataset ${DATASET} \
-#     --model ${MODELNAME} \
-#     --work_space ${WORKDIR} \
-#     --train_list ${VALID} \
-#     --input_size ${INPUTSIZE} \
-#     --cam_out_dir ${SEGDIR} \
-#     --checkpoint results_voc/mcta/mcta_deit_small-7458.pth \
+# ============= Make Class Activation Maps of Model ============= #
+python make_cam.py \
+    --dataset ${DATASET} \
+    --model ${MODELNAME} \
+    --work_space ${WORKDIR} \
+    --train_list ${VALID} \
+    --input_size ${INPUTSIZE} \
+    --cam_out_dir ${SEGDIR} \
+    --checkpoint results_voc/mcta/mcta-deit-small-voc-7458.pth \
       
 # # ============= Evaluate Class Activation Maps No CRF =============#
 # python eval_cam_crf.py \
@@ -36,19 +36,19 @@ CUDA_VISIBLE_DEVICES=${GPU}
 #     --id_list ${VALID} \
 #     --curve_threshold \
 
-# # ============= Evaluate Class Activation Maps =============#
-# python eval_cam_crf.py \
-#     --use_crf \
-#     --dataset ${DATASET} \
-#     --work_space ${WORKDIR} \
-#     --eval_cam_dir ${SEGDIR} \
-#     --crf_cam_dir ${CRFDIR} \
-#     --id_list ${VALID} \
+# ============= Evaluate Class Activation Maps =============#
+python eval_cam_crf.py \
+    --use_crf \
+    --dataset ${DATASET} \
+    --work_space ${WORKDIR} \
+    --eval_cam_dir ${SEGDIR} \
+    --crf_cam_dir ${CRFDIR} \
+    --id_list ${VALID} \
 
 #============= Evaluate =============#
 python steps_voc/eval_sem_seg.py \
     --work_space ${WORKDIR} \
-    --seg_out_dir crf_pred_multi \
+    --seg_out_dir ${CRFDIR} \
     --infer_list ${VALID} \
 
 # # Save the generated mask to zip
